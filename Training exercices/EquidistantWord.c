1f3e0d205c3b8278579fb577707db1b983ae866d
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-07 22:46:57 -0300

    Refactoring
    Refactoring the code just to format it in a better way and specify more clearly how it works.

---
+++ Training exercices/EquidistantWord.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (void){
+    char word[40]={};
+    int newLetter, i;
+    system("cls");
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("This program takes your word and changes each letter to the equidistant one in relation to the\nmiddle of the alphabet\n");
+    printf("-------------------------------------------------------------------------------------------------\n");
+    printf("Type a word: ");
+    gets(word);
+    for(i=0; i<sizeof(word); i++){
+        if(word[i]!=NULL){
+            if(('a'>word[i] || word[i]>'z')&&('A'>word[i] || word[i]>'Z')){
+                printf("Input not valid. Please, type a word: ");
+                gets(word);
+                i=0;
+            }
+        }
+    }
+    printf("Resulting word: ");
+    for(i=0; i<sizeof(word); i++){
+        if('a'<=word[i] && word[i]<='z'){
+            newLetter = 'n' - word[i];
+            newLetter = 'n' + newLetter - 1;
+            word[i] = newLetter;
+        }
+        if('A'<=word[i] && word[i]<='Z'){
+            newLetter = 'N' - word[i];
+            newLetter = 'N' + newLetter - 1;
+            word[i] = newLetter;
+        }
+        printf("%c", word[i]);
+    }
+    printf("\n");
+    system("pause");
+}
